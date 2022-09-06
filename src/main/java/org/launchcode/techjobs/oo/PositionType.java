0e6e3b54705b66df7e9c7df11e6ab3d0ba7c78b5
Calderon, Falon <Falon_Calderon@cable.comcast.com>
2022-09-06 17:01:28 -0400

    TechJobs2 Assignment2

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -2,36 +2,22 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class PositionType {
-
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public PositionType() {
-        id = nextId;
-        nextId++;
-    }
+public class PositionType extends JobField {
 
     public PositionType(String value) {
-        this();
-        this.value = value;
+        super(value);
     }
-    public String toString() {
-        return value;
-    }
-
-    @Override
+   @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PositionType that = (PositionType) o;
-        return id == that.id;
+        return getId() == PositionType.getNextId();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, value);
+        return Objects.hash(getId());
     }
 // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
@@ -39,17 +25,5 @@ public class PositionType {
     //  their id fields match.
 
     // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
 
 }
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,71 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertNotEquals(job1.getId(), job2.getId());
 
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job3 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals("Product Tester", job3.getName());
+        assertEquals("ACME", job3.getEmployer().getValue());
+        assertEquals("Desert", job3.getLocation().getValue());
+        assertEquals("Quality control", job3.getPositionType().getValue());
+        assertEquals("Persistence", job3.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job job4 = new Job("Product Tester", new Employer("ACME"),new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job5 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+       // assertNotEquals(job4.getEmployer().getValue(), job5.getEmployer().getValue());
+// expect job4 not to equal job5
+       assertFalse(job4.equals(job5));
+    }
+
+    @Test
+    public void testToStringContainsCorrectLabelsAndData() {
+        Job job6 = new Job("Product Tester", new Employer("ACME"),new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        // hardcode a string that looks like what we expect (does job6 string equal what we created?)
+        String expectedString = "\n" + "ID: " + job6.getId() + "\n" +
+                "Name: Product Tester" + "\n" +
+                "Employer: ACME" + "\n" +
+                "Location: Desert" + "\n" +
+                "Position Type: Quality control" + "\n" +
+                "Core Competency: Persistence" + "\n";
+        String actualString = job6.toString();
+        assertEquals(expectedString, actualString);
+
+    }
+
+    @Test
+    public void testToStringHandlesEmptyField() {
+        Job job7 = new Job("", new Employer(""),new Location(""), new PositionType("Quality control"), new CoreCompetency("Persistence" ));
+
+        String expectedString = "\n" + "ID: " + job7.getId() + "\n" +
+                "Name: Data not available" + "\n" +
+                "Employer: Data not available" + "\n" +
+                "Location: Data not available" + "\n" +
+                "Position Type: Quality control" + "\n" +
+                "Core Competency: Persistence" + "\n";
+        String actualString = job7.toString();
+        assertEquals(expectedString, actualString);
+    }
 }
